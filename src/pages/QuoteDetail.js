50e1e74e7fa2c74b09bcc9bbfdd5ece1872f81a5
AdaY45 <afsdert2012@gmail.com>
2021-07-29 20:34:44 +0300

    Added React Routing, http requests

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,16 +1,52 @@
-import { useParams, Route } from "react-router";
+import { useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
-    const params = useParams();
-    const url = `/quotes/${params.quoteId}/comments`;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+  const match = useRouteMatch();
+  const params = useParams();
+  const { quoteId } = params;
+  
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return <div className="centered">
+      <LoadingSpinner />
+    </div>
+  }
+
+  if(error) {
+    return <p className="centered focus">{error}</p>
+  }
+
+  if(!loadedQuote.text) {
+    return <p>No Quote Found!</p>;
+  }
 
   return (
     <section>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route path={url}>
-          <Comments />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
       </Route>
     </section>
   );
